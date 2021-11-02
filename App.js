4975fa5c6e5a82fd74bf86f145caa12c65e6ace1
yo <chutimab755@gmail.com>
2021-11-02 12:00:14 +0700

    z

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
-import { Ionicons, MaterialCommunityIcons } from '@expo/vector-icons';
+import { Ionicons, MaterialCommunityIcons, FontAwesome } from '@expo/vector-icons';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { createStackNavigator} from '@react-navigation/stack';
-import { View, Text, StyleSheet, TextInput } from 'react-native';
+import {Button, View, Text, StyleSheet, TextInput,Image } from 'react-native';
 
 const Tab = createBottomTabNavigator();
 const Drawer = createDrawerNavigator();
@@ -15,7 +15,7 @@ return (
     <Tab.Navigator
     
         screenOptions={({ route }) => ({
-          tabBarActiveTintColor: '#e91e63',
+          tabBarActiveTintColor: '#22b6c7',
           tabBarIcon: ({ focused, color, size }) => {
             let iconName;
 
@@ -27,8 +27,11 @@ return (
               iconName = focused ? 'apps' : 'apps-outline';
             } else if (route.name === 'ติดต่อเรา') {
               iconName = focused ? 'ios-call' : 'ios-call-outline';
+            } else if (route.name === 'สมัครสมาชิก') {
+              iconName = focused ? 'add-circle' : 'add-circle-outline';
+            } else if (route.name === 'เข้าสู่ระบบ') {
+              iconName = focused ? 'body' : 'body-outline';
             }
-
             // You can return any component that you like here!
             return <Ionicons name={iconName} size={size} color={"green"} />;
           },
@@ -42,6 +45,8 @@ return (
           <Tab.Screen name="เกี่ยวกับเรา" component={TabB} options={{ headerShown: false }}/>
           <Tab.Screen name="บริการของเรา" component={TabC} options={{ headerShown: false }}/>
           <Tab.Screen name="ติดต่อเรา" component={TabD} options={{ headerShown: false }}/>
+          <Tab.Screen name="สมัครสมาชิก" component={TabE} options={{ headerShown: false }}/>
+          <Tab.Screen name="เข้าสู่ระบบ" component={TabF} options={{ headerShown: false }}/>
     </Tab.Navigator>
 );
 }
@@ -50,8 +55,16 @@ function TabA({navigation}) {
   return (
     <View style={{ flex: 1, justifyContent: 'center',  alignItems: 'center' }}>
       <Text>
-        ยินดีต้อนรับสู่หน้าหลักจ้าว
+        ยินดีต้อนรับสู่หน้าหลักเนอะจ้าว
       </Text>
+      <Image
+    source={require('./assets/123.jpg')}
+    style={{
+                  width: 200,
+                  height: 330,
+                  borderRadius: 200,
+                }}
+    />
     </View>
   );
 }
@@ -61,7 +74,7 @@ function TabB({navigation}) {
   return (
     <View style={{ flex: 1, justifyContent: 'center',  alignItems: 'center' }}>
       <Text>
-      ยินดีต้อนรับสู่หน้าข้อมูลเกี่ยวกับแอบจ้าว
+      ยินดีต้อนรับเข้าสู่ข้อมูลเกี่ยวกับเราเนอะจ้าว
       </Text>
     </View>
   );
@@ -71,8 +84,16 @@ function TabC({navigation}) {
   return (
     <View style={{ flex: 1, justifyContent: 'center',  alignItems: 'center' }}>
       <Text>
-      ยินดีต้อนรับสู่หน้าบริการจ้าว
+      ยินดีต้อนรับสู่หน้าการบริการของทางเราเนอะจ้าว
       </Text>
+      <Image
+    source={require('./assets/Ganyu.png')}
+    style={{
+                  width: 200,
+                  height: 330,
+                  borderRadius: 200,
+                }}
+    />
     </View>
   );
 }
@@ -81,13 +102,119 @@ function TabD({navigation}) {
   return (
     <View style={{ flex: 1, justifyContent: 'center',  alignItems: 'center' }}>
       <Text>
-        ยินดีต้อนรับสู่หน้าการติดต่อของเราจ้าว
+        ยินดีต้อนรับเข้าสู่หน้าการติดต่อเนอะจ้าว
       </Text>
     </View>
   );
 }
 
+function TabE({navigation}) {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center',  alignItems: 'center' }}>
+      <Text>
+        ยินดีต้อนรับเข้าสู่หน้าการสมัครเนอะจ้าว
+      </Text>
+      <TextInput
+    
+        onChangeText ={username=>this.setState({username})}
+            placeholder="  ชื่อ" 
+          style={{
+            backgroundColor: '#F5DEB3',
+            padding: 10,
+            width: '60%',
+            marginTop: 20,
+            fontSize: 15,
+            borderRadius : 15,
 
+          }}
+        />
+        <TextInput
+        onChangeText ={username=>this.setState({username})}
+          placeholder="อีเมล"
+          style={{
+            backgroundColor: '#F5DEB3',
+            padding: 10,
+            width: '60%',
+            marginTop: 15,
+            fontSize: 15,
+            borderRadius : 1,
+            
+          }}
+        />
+        <TextInput
+        onChangeText ={username=>this.setState({username})}
+          placeholder="เบอร์โทรศัพ"
+          style={{
+            backgroundColor: '#F5DEB3',
+            padding: 10,
+            width: '60%',
+            marginTop: 15,
+            fontSize: 15,
+            borderRadius : 15,
+            
+          }}
+        />
+        <TextInput
+        onChangeText ={username=>this.setState({username})}
+          placeholder="ที่อยู่"
+          style={{
+            backgroundColor: '#F5DEB3',
+            padding: 10,
+            width: '60%',
+            marginTop: 15,
+            fontSize: 15,
+            borderRadius : 1,
+           
+          }}
+        />
+   
+          <TextInput
+        onChangeText={password=>this.setState({password})}
+          placeholder="รหัสผ่าน"
+          secureTextEntry={true}
+          style={{
+            backgroundColor: '#F5DEB3',
+            padding: 10,
+            width: '60%',
+            marginTop: 15,
+            fontSize: 15,
+            borderRadius : 1,
+    
+          }}
+        />
+    </View>
+  );
+}
+
+function TabF({navigation}) {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center',  alignItems: 'center' }}>
+      <Text>
+        ยินดีต้อนรับเข้าสู่หน้าการเข้าสู่ระบบเนอะจ้าว
+      </Text>
+      <TextInput
+      style={styles.input}
+      onChangeText={text => onChangeText(text)}
+      value={""}
+      placeholder={'Username'}
+    />
+
+    <TextInput
+      style={styles.input}
+      onChangeText={text => onChangeText(text)}
+      value={""}
+      placeholder={'Password'}
+    />
+
+    <Button
+          title={'Login'}
+          style={styles.input}
+          onPress={""}
+    />
+    </View>
+  );
+  
+}
 
 
 export default function App() {
@@ -96,10 +223,11 @@ return(
     <NavigationContainer>
       <Drawer.Navigator initialRouteName="Home">
         <Drawer.Screen name="หน้าแรก" component={HomeScreen}/>
-        <Drawer.Screen name="เกี่ยวกับเรา" component={""} />
-        <Drawer.Screen name="บริการของเรา" component={""} />
-        <Drawer.Screen name="ติดต่อเรา" component={""} />
-        <Drawer.Screen name="เข้าสู่ระบบ" component={""} />
+        <Drawer.Screen name="เกี่ยวกับเรา" component={TabB} />
+        <Drawer.Screen name="บริการของเรา" component={TabC} />
+        <Drawer.Screen name="ติดต่อเรา" component={TabD} />
+        <Drawer.Screen name="สมัครสมาชิก" component={TabE} />
+        <Drawer.Screen name="เข้าสู่ระบบ" component={TabF} />
       </Drawer.Navigator>
     </NavigationContainer>
 
@@ -110,3 +238,19 @@ return(
   */
 );
 }
+const styles = StyleSheet.create({
+  Container : {
+    backgroundColor: 'skyblue',
+    flex: 1,
+    textAlign: 'center',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  TextInput: {
+    backgroundColor: '#FFF',
+    width: 200,
+    borderRadius: 20,
+    padding: 5,
+  },
+ }
+ );
